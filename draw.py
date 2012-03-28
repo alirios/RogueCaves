6d0eec03530cfc438de8da6858d0b181756c8e86
flags <flags@srsfckn.biz>
2012-03-28 12:52:04 -0400

    Vision

--- draw.py
+++ draw.py
@@ -1,3 +1,59 @@
+class line_diag:
+	def __init__(self, start, end):
+		self.path = []
+		if start == end: return None
+		
+		self.start = list(start)
+		self.end = list(end)
+		
+		self.steep = abs(self.end[1]-self.start[1]) > abs(self.end[0]-self.start[0])
+		
+		if self.steep:
+			self.start = self.swap(self.start[0],self.start[1])
+			self.end = self.swap(self.end[0],self.end[1])
+		
+		if self.start[0] > self.end[0]:
+			self.start[0],self.end[0] = self.swap(self.start[0],self.end[0])		
+			self.start[1],self.end[1] = self.swap(self.start[1],self.end[1])
+		
+		dx = self.end[0] - self.start[0]
+		dy = abs(self.end[1] - self.start[1])
+		error = 0
+		
+		try:
+			derr = dy/float(dx)
+		except:
+			return None
+		
+		ystep = 0
+		y = self.start[1]
+		
+		if self.start[1] < self.end[1]: ystep = 1
+		else: ystep = -1
+		
+		for x in range(self.start[0],self.end[0]+1):
+			if self.steep:
+				self.path.append((y,x))
+			else:
+				self.path.append((x,y))
+			
+			error += derr
+			
+			if error >= 0.5:
+				y += ystep
+				error -= 1.0
+		
+		if not self.path[0] == (start[0],start[1]):
+			self.path.reverse()
+	
+	def swap(self,n1,n2):
+		return [n2,n1]
+
+def draw_diag_line(start,end):
+	_l = line_diag(start,end)
+	
+	return _l.path
+
 def draw_line(pos1,pos2,diag=False,includeend=False):
 	if diag:
 		_l = draw_line_diag(pos1,pos2)
--- life.py
+++ life.py
@@ -1,4 +1,4 @@
-import var
+import draw, var
 
 class life:
 	def __init__(self,player=False):
@@ -10,10 +10,28 @@ class life:
 		self.name = 'Default'
 		self.hp = 10
 		self.pos = [0,0]
-		
+		self.seen = []
 		self.alignment = 'neutral'
 		
 		var.life.append(self)
+	
+	def think(self):
+		self.seen = []
+		
+		for life in var.life:
+			if life == self: continue
+			
+			_l = draw.draw_diag_line(self.pos,life.pos)
+			
+			_seen = True
+			for pos in _l:
+				if not self.level.map[pos[0]][pos[1]]:
+					_seen = False
+					break
+			
+			if _seen:
+				self.seen.append({'who':life,'dist':len(_l)})
+				print 'Sees you!'
 	
 	def walk(self,dir):
 		if self.player:
@@ -25,6 +43,8 @@ class life:
 				self.pos[0]-=1
 			elif dir == 'right' and self.level.map[self.pos[0]+1][self.pos[1]]:
 				self.pos[0]+=1
+		else:
+			self.think()
 
 class human(life):
 	def __init__(self,player=False):
